50ea328b4f8f9f6e7a09df9ac7f0cf22b8451975
Ivan <ivanmontesdeoca00@icloud.com>
2026-03-26 15:49:51 -0300

    fix bucles practica

---
+++ bucle_edad.py
@@ -0,0 +1,3 @@
+edad = int(input("Cuantos años tenes? : "))
+for i in range(edad):
+    print("Has cumplido " + str(i+1) + " años")
---
+++ recorredor_palabras.py
@@ -0,0 +1,7 @@
+palabra = input("Ingrese su palabra deseada: ")
+contador = 1
+
+while contador <= 10:
+    print(f"se recorrio una ves la {palabra}")
+    contador += 1
+print(f"se recorrio 10 veces la {palabra}")
