56f63f51f8bb81ee5aff43d27bad79ec9d5b849f
Ivan Montes de Oca <ivanmontesdeoca00@icloud.com>
2026-03-31 22:22:25 -0300

    ultima parte de la clase ejercicios

---
+++ validarEdad.py
@@ -0,0 +1,8 @@
+while True:
+    edad = int(input("Ingrese su edad por favor: "))
+    if edad < 18:
+        print("Acceso denegado. Debes ser mayor de edad.")
+    else:
+        print("Bienvenido al sistema!!!")
+        break
+    
