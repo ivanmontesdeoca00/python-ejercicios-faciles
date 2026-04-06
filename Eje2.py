83a98e48de6a24fb63ae669665b86bcd797bd3f4
Ivan <ivanmontesdeoca00@icloud.com>
2026-04-05 23:36:14 -0400

    practica rapidita de bucle

---
+++ Eje2.py
@@ -0,0 +1,4 @@
+edad = int(input("¿Que edad tenes?: "))
+
+for i in range(edad):
+    print("Cumpliste " + str(i+1) +" años.")
---
+++ eje4.py
@@ -0,0 +1,3 @@
+numero = int(input("Pone un numero entero: "))
+for resta in range(numero, -1, -1):
+    print(resta, end=", ")
